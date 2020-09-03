12ef305aeec73974d5cb09e48a8eded7a06ab1b5
Jack Shields <jshields405@gmail.com>
2020-09-03 21:25:15 +0000

    even more changes

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ elif (gradepoint2 == 'D' or gradepoint2 == 'd'):
     print("Grade point for course 2 is: 1.0")
     gradepoint2 = 1.0
 else:
-    print("Grade point for course 1 is: 0.0")
+    print("Grade point for course 3 is: 0.0")
     gradepoint2 = 0.0
 
 gradepoint3 = input("Enter your course 3 letter grade: ")
@@ -90,7 +90,7 @@ elif (gradepoint3 == 'D' or gradepoint3 == 'd'):
     print("Grade point for course 3 is: 1.0")
     gradepoint3 = 1.0
 else:
-    print("Grade point for course 1 is: 0.0")
+    print("Grade point for course 3 is: 0.0")
     gradepoint3 = 0.0
 
 credit1 = float(credit1)
@@ -99,4 +99,4 @@ credit3 = float(credit3)
 
 GPA = (gradepoint1 * credit1 + gradepoint2 * credit2 +
        gradepoint3 * credit3) / (credit1 + credit2 + credit3)
-print(f"\nYour GPA is: {GPA}")
+print(f"Your GPA is: {GPA}")
