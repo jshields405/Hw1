a8124609f171bcdc6b215ad470fd17fde59df64f
Jack Shields <jshields405@gmail.com>
2020-09-03 21:50:09 +0000

    rid of or statements

--- main.py
+++ main.py
@@ -3,28 +3,28 @@
 gradepoint1 = input("Enter your course 1 letter grade: ")
 credit1 = input("Enter your course 1 credit: ")
 
-if (gradepoint1 == 'A' or gradepoint1 == 'a'):
+if (gradepoint1 == 'A'):
     print("Grade point for course 1 is: 4.0")
     gradepoint1 = 4.0
-elif (gradepoint1 == 'A-' or gradepoint1 == 'a-'):
+elif (gradepoint1 == 'A-'):
     print("Grade point for course 1 is: 3.67")
     gradepoint1 = 3.67
-elif (gradepoint1 == 'B+' or gradepoint1 == 'b+'):
+elif (gradepoint1 == 'B+'):
     print("Grade point for course 1 is: 3.33")
     gradepoint1 = 3.33
-elif (gradepoint1 == 'B' or gradepoint1 == 'b'):
+elif (gradepoint1 == 'B'):
     print("Grade point for course 1 is: 3.0")
     gradepoint1 = 3.0
-elif (gradepoint1 == 'B-' or gradepoint1 == 'b-'):
+elif (gradepoint1 == 'B-'):
     print("Grade point for course 1 is: 2.67")
     gradepoint1 = 2.67
-elif (gradepoint1 == 'C+' or gradepoint1 == 'c+'):
+elif (gradepoint1 == 'C+'):
     print("Grade point for course 1 is: 2.33")
     gradepoint1 = 2.33
-elif (gradepoint1 == 'C' or gradepoint1 == 'c'):
+elif (gradepoint1 == 'C'):
     print("Grade point for course 1 is: 2.0")
     gradepoint1 = 2.0
-elif (gradepoint1 == 'D' or gradepoint1 == 'd'):
+elif (gradepoint1 == 'D'):
     print("Grade point for course 1 is: 1.0")
     gradepoint1 = 1.0
 else:
@@ -34,28 +34,28 @@ else:
 gradepoint2 = input("Enter your course 2 letter grade: ")
 credit2 = input("Enter your course 2 credit: ")
 
-if (gradepoint2 == 'A' or gradepoint2 == 'a'):
+if (gradepoint2 == 'A'):
     print("Grade point for course 2 is: 4.0")
     gradepoint1 = 4.0
-elif (gradepoint2 == 'A-' or gradepoint2 == 'a-'):
+elif (gradepoint2 == 'A-'):
     print("Grade point for course 2 is: 3.67")
     gradepoint2 = 3.67
-elif (gradepoint2 == 'B+' or gradepoint2 == 'b+'):
+elif (gradepoint2 == 'B+'):
     print("Grade point for course 2 is: 3.33")
     gradepoint2 = 3.33
-elif (gradepoint2 == 'B' or gradepoint2 == 'b'):
+elif (gradepoint2 == 'B'):
     print("Grade point for course 2 is: 3.0")
     gradepoint2 = 3.0
-elif (gradepoint2 == 'B-' or gradepoint2 == 'b-'):
+elif (gradepoint2 == 'B-'):
     print("Grade point for course 2 is: 2.67")
     gradepoint2 = 2.67
-elif (gradepoint2 == 'C+' or gradepoint2 == 'c+'):
+elif (gradepoint2 == 'C+'):
     print("Grade point for course 2 is: 2.33")
     gradepoint2 = 2.33
-elif (gradepoint2 == 'C' or gradepoint2 == 'c'):
+elif (gradepoint2 == 'C'):
     print("Grade point for course 2 is: 2.0")
     gradepoint2 = 2.0
-elif (gradepoint2 == 'D' or gradepoint2 == 'd'):
+elif (gradepoint2 == 'D'):
     print("Grade point for course 2 is: 1.0")
     gradepoint2 = 1.0
 else:
@@ -65,28 +65,28 @@ else:
 gradepoint3 = input("Enter your course 3 letter grade: ")
 credit3 = input("Enter your course 3 credit: ")
 
-if (gradepoint3 == 'A' or gradepoint3 == 'a'):
+if (gradepoint3 == 'A'):
     print("Grade point for course 3 is: 4.0")
     gradepoint3 = 4.0
-elif (gradepoint3 == 'A-' or gradepoint3 == 'a-'):
+elif (gradepoint3 == 'A-'):
     print("Grade point for course 3 is: 3.67")
     gradepoint3 = 3.67
-elif (gradepoint3 == 'B+' or gradepoint3 == 'b+'):
+elif (gradepoint3 == 'B+'):
     print("Grade point for course 3 is: 3.33")
     gradepoint3 = 3.33
-elif (gradepoint3 == 'B' or gradepoint3 == 'b'):
+elif (gradepoint3 == 'B'):
     print("Grade point for course 3 is: 3.0")
     gradepoint3 = 3.0
-elif (gradepoint3 == 'B-' or gradepoint3 == 'b-'):
+elif (gradepoint3 == 'B-'):
     print("Grade point for course 3 is: 2.67")
     gradepoint3 = 2.67
-elif (gradepoint3 == 'C+' or gradepoint3 == 'c+'):
+elif (gradepoint3 == 'C+'):
     print("Grade point for course 3 is: 2.33")
     gradepoint3 = 2.33
-elif (gradepoint3 == 'C' or gradepoint3 == 'c'):
+elif (gradepoint3 == 'C'):
     print("Grade point for course 3 is: 2.0")
     gradepoint3 = 2.0
-elif (gradepoint3 == 'D' or gradepoint3 == 'd'):
+elif (gradepoint3 == 'D'):
     print("Grade point for course 3 is: 1.0")
     gradepoint3 = 1.0
 else:
